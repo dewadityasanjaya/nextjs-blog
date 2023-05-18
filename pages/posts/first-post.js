2264c8c6a3a1869ae4ee41eef53f243b78798ee3
Dewa Aditya <dewadityasanjaya@gmail.com>
2023-05-18 16:32:22 +0700

    add asset and third party script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,18 @@
-import Link from "next/Link";
+import Link from 'next/Link';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First Post</title>
+                <Script
+                    src="https://example.com/samplescript.js"
+                    strategy="lazyOnload"
+                    onLoad={() => { console.log('script loaded correctly, window.FB has been populated') }}
+                />
+            </Head>
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
